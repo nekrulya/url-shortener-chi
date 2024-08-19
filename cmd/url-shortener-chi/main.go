6b48fdeb853f4bc3bb0121e1a429d2c1c818fe77
nekrulya <nekrulyaa@mail.ru>
2024-08-19 11:42:36 +0500

    start logger, storage added

---
+++ cmd/url-shortener-chi/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/nekrulya/url-shortener-chi/internal/config"
+	"github.com/nekrulya/url-shortener-chi/internal/lib/logger/sl"
+	"github.com/nekrulya/url-shortener-chi/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	// TODO: init logger: slog
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	// TODO: init storage: sqlite
+	// storage, err := sqlite.New(cfg.StoragePath)
+	_, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	// TODO: init router: chi, render
+
+	// TODO: init server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
